f8937f340689f8f99086cb2f4833019e0f487943
Pablo Enoc <pablo@enocc.com>
2026-01-11 16:52:42 -0800

    bump stroke width on settings icon

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ foreach($urls as $url) {
 		     viewBox="0 0 24 24"
 		     fill="none"
 		     stroke="currentColor"
-		     stroke-width="1"
+		     stroke-width="1.5"
 		     stroke-linecap="round"
 		     stroke-linejoin="round"
 		>
