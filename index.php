f1736ad9111c37d5a460e6b8b8246781d94a4045
Pablo Enoc <pablo@enocc.com>
2026-01-14 14:30:11 -0800

    Add date and time to each post

--- index.php
+++ index.php
@@ -163,6 +163,7 @@ unicode: "f7b3"
 				<?php else: ?>
 					<?php foreach($feed['entries'] as $entry): ?>
 						<div class="post">
+							<p class="post__date"><?= htmlspecialchars(date("d M · h:i a", $entry['published_date'])) ?></p>
 							<p class="post__title"><a target="_blank" href="<?= htmlspecialchars($entry['url']) ?>" class="post__link"><?= htmlspecialchars($entry['title']); ?></a></p>
 						</div>
 					<?php endforeach; ?>
