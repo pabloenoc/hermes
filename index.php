f4eecd37e755dc6dee54d8bb93230dc419588d43
Pablo Enoc <pablo@enocc.com>
2026-01-16 13:17:53 -0800

    spacing

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-// TODO: Fetch feed updates
-
 date_default_timezone_set('America/Los_Angeles');
-
 
 $db = new SQLITE3('./db/hrmss.sqlite');
 $result = $db->query('
@@ -13,8 +10,8 @@ $result = $db->query('
 
 $feeds = []; 
 while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-    $row['entries'] = []; // prepare slot so I can do e.g. $feed['entries'] to get array of entries
-    $feeds[$row['id']] = $row; // make id=key so I can do $feeds[32] for item with id 32...
+    $row['entries'] = []; // $feed['entries'] to get array of entries
+    $feeds[$row['id']] = $row; // $feeds[32] for item with id 32...
 }
 
 // ENTRIES
@@ -107,10 +104,9 @@ while($row = $result->fetchArray(SQLITE3_ASSOC)) {
 			>
 			<path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
 			<path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
-		</svg>
-	</a>
-
-</navbar>
+			</svg>
+		</a>
+	</navbar>
 
 <?php if (count($feeds) === 0): ?>
 	<main style="text-align: center;">
