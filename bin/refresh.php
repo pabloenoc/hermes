b9ae3a091ed17008b76ecc7eed32b19dda0d8b8b
Pablo Enoc <pablo@enocc.com>
2026-01-22 20:16:52 -0800

    refactor

--- bin/refresh.php
+++ bin/refresh.php
@@ -18,75 +18,50 @@ while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
     $feeds[] = $row;
 }
 
+function save_feed_entry($db, $feed_id, $title, $published_date, $guid, $url) {
+    $stmt = $db->prepare('INSERT OR IGNORE INTO entries (feed_id, title, published_date, guid, url) VALUES (:feed_id, :title, :published_date, :guid, :url)');
+    $stmt->bindValue(':feed_id', $feed_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':title', $title, SQLITE3_TEXT);
+    $stmt->bindValue(':published_date', $published_date, SQLITE3_INTEGER);
+    $stmt->bindValue(':guid', $guid, SQLITE3_TEXT);
+    $stmt->bindValue(':url', $url, SQLITE3_TEXT);
+    $stmt->execute();
+}
+
 foreach ($feeds as $feed) {
     echo "\nRefreshing {$feed['url']} ...\n";
 
+    $feed_id = $feed['id'];
+
     try {
         $parsed_feed = Feed::load($feed['url']);
-        $feed_id = $feed['id'];
+    }
+    catch(Exception $e) {
+        echo $e->getMessage();
+        continue;
+    }
 
-        if ($feed['format'] === 'atom') {
-            foreach ($parsed_feed->entry as $entry) {
-                if (empty($entry->id)) { // ?? this skips if no url
-                    continue;
-                }
+    if ($feed['format'] === 'rss') {
+        foreach($parsed_feed->item as $item) {
+            $title = $item->title;
+            $published_date = strtotime($item->pubDate);
+            $guid = $item->guid;
+            $url = $item->link;
 
-                // Prep Variables
-                $title = $entry->title;
-                $published_date = (int)$entry->timestamp;
-                $guid = $entry->id;
-                $url = $entry->link['href'];
-
-                $stmt = $db->prepare('
-                    INSERT OR IGNORE INTO entries
-                    (feed_id, title, published_date, guid, url)
-                    VALUES (:feed_id, :title, :published_date, :guid, :url)
-                    ');
-
-                $stmt->bindValue(':feed_id', $feed_id, SQLITE3_INTEGER);
-                $stmt->bindValue(':title', $title, SQLITE3_TEXT);
-                $stmt->bindValue(':published_date', $published_date, SQLITE3_INTEGER);
-                $stmt->bindValue(':guid', $guid, SQLITE3_TEXT);                                  
-                $stmt->bindValue(':url', $url, SQLITE3_TEXT);
-                $stmt->execute();
-            }
+            save_feed_entry($db, $feed_id, $title, $published_date, $guid, $url);
         }
+    }
 
-        if ($feed['format'] === 'rss') {
-           foreach ($parsed_feed->item as $item) {
-              $title = $item->title;
-              $published_date = strtotime($item->pubDate);
-              $guid = $item->guid;
-              $url = $item->link;
+    if ($feed['format'] === 'atom') {
+        foreach($parsed_feed->entry as $entry) {
+            $title = $entry->title;
+            $published_date = (int)$entry->timestamp;
+            $guid = $entry->id;
+            $url = $entry->link['href'];
 
-              $stmt = $db->prepare('INSERT OR IGNORE INTO entries
-                (feed_id, title, published_date, guid, url)
-                VALUES (:feed_id, :title, :published_date, :guid, :url)
-                ');
-
-              $stmt->bindValue(':feed_id', $feed_id, SQLITE3_INTEGER);
-              $stmt->bindValue(':title', $title, SQLITE3_TEXT);
-              $stmt->bindValue(':published_date', $published_date, SQLITE3_INTEGER);
-              $stmt->bindValue(':guid', $guid, SQLITE3_TEXT);
-              $stmt->bindValue(':url', $url, SQLITE3_TEXT);
-              $stmt->execute();
-          }
+            save_feed_entry($db, $feed_id, $title, $published_date, $guid, $url);
         }
-
-    	// Update last_fetched_at timestamp in feeds table
-      $stmt = $db->prepare('
-       UPDATE feeds
-           SET last_fetched_at = CURRENT_TIMESTAMP
-           WHERE id = :id
-           ');
-
-      $stmt->bindValue(':id', $feed_id, SQLITE3_INTEGER);
-      $stmt->execute();
-
-  } catch (Throwable $t) {
-    fwrite(STDERR, "Failed: {$feed['url']}\n");
-}
-
+    }
 }
 
 echo "\nFinished.\n";
