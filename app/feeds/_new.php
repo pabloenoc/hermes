763add47e4e637146f14f0766bbb9b95cfe4e85b
Pablo Enoc <pablo@enocc.com>
2026-01-24 00:56:21 -0800

    fix: get entry date in atom formats

--- app/feeds/_new.php
+++ app/feeds/_new.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_feed_url']))
 
         if ($feed_format === 'rss') {
             foreach($feed->item as $item) {
-                $title = $item->title ?? 'A post from ' . $feed->title;
+                $title = $item->title ?? 'Post via ' . $feed->title;
                 $published_date = strtotime($item->pubDate);
                 $guid = $item->guid;
                 $url = $item->link;
@@ -70,8 +70,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_feed_url']))
 
         if ($feed_format === 'atom') {
             foreach($feed->entry as $entry) {
-                $title = $entry->title ?? 'A post from ' . $feed->title;
-                $published_date = (int)$entry->timestamp; // TODO: fix if empty
+                $title = $entry->title ?? 'Post via ' . $feed->title;
+                $published_date = (int)$entry->published;
                 $guid = $entry->id;
                 $url = $entry->link['href'];
 
