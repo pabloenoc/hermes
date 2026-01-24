8eae3540c4969463d0b1d25fe47c44546366fb85
Pablo Enoc <pablo@enocc.com>
2026-01-23 18:24:33 -0800

    extract _head.php

---
+++ app/shared/_head.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+<head>
+    <title>Hermes - Read the web</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="Hermes" />
+    <link rel="manifest" href="/site.webmanifest" />
+
+    <meta property="og:title" content="Hermes - Read the web">
+    <meta property="og:description" content="Keep an eye on what matters to you.">
+    <meta property="og:image" content="/images/og_image.png">
+    <meta property="og:url" content="https://hrmss.enocc.com">
+    <meta property="og:type" content="website">
+
+</head>
+<body>
