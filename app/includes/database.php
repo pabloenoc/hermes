feabfbe7bed6c96603c37f1c27b82aaa04105e55
Pablo Enoc <pablo@enocc.com>
2026-01-29 10:32:03 -0800

    set db path

--- app/includes/database.php
+++ app/includes/database.php
@@ -1,4 +1,10 @@
 <?php
 
+require_once(__DIR__.'/../../vendor/autoload.php');
+
+use \Hermes\Feed;
+
 $db = new SQLITE3(__DIR__.'/../../db/hrmss.sqlite');
 $db->exec('PRAGMA foreign_keys = ON;');
+
+Feed::set_database($db);
